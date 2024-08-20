d65760753cdecacee379608b9004ff3f5c9da470
neerajbaloni <neerajbaloni@gmail.com>
2024-08-20 03:53:54 -0400

    code for cart count

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,13 +1,14 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addItem } from './CartSlice';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
     const dispatch = useDispatch();
+    const cartItems = useSelector(state => state.cart.items);
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -235,6 +236,12 @@ function ProductList() {
     fontSize: '30px',
     textDecoration: 'none',
    }
+   const cStyle = {
+    display:'inline-block',
+    position: 'absolute',
+    left: '30px',
+    top: '20px'
+   }
    const handleCartClick = (e) => {
     e.preventDefault();
     setShowCart(true); // Set showCart to true when cart icon is clicked
@@ -251,12 +258,21 @@ const handlePlantsClick = (e) => {
   };
 
   const handleAddToCart = (product) => {
+    if(addedToCart[product.name]) return;
     dispatch(addItem(product));
     setAddedToCart((prevState) => ({
        ...prevState,
        [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
      }));
   };
+  const calculateItemCount = () => {
+    console.log(cartItems);
+    let count = 0
+    cartItems.forEach( item=> {
+        count += item.quantity;
+    })
+    return count
+  }
     return (
         <div>
              <div className="navbar" style={styleObj}>
@@ -274,8 +290,12 @@ const handlePlantsClick = (e) => {
             </div>
             <div style={styleObjUl}>
                 <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                <div> 
+                <div style={{"position": 'relative'}}> 
                     <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                    {calculateItemCount() ? 
+                        (<span style={cStyle}>{calculateItemCount()}</span>)
+                        : ('')
+                    }
                         <h1 className='cart'>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>
                         </h1>
@@ -293,7 +313,7 @@ const handlePlantsClick = (e) => {
                                 <img className="product-image" src={plant.image} alt={plant.name} />
                                 <div className="product-title">{plant.name}</div>
                                 {/*Similarly like the above plant.name show other details like description and cost*/}
-                                <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                <button  className={"product-button " + (addedToCart[plant.name] ? 'added-to-cart' : '')} onClick={() => handleAddToCart(plant)}>{addedToCart[plant.name] ? 'Added to Cart' :'Add to Cart'}</button>
                             </div>
                             ))}
                         </div>
